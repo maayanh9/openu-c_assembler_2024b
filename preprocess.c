81ed575deafb7980bc04b1ac7c0794a7e164579a
maayanh9 <maayanh9@gmail.com>
2024-08-22 00:38:05 +0300

    bug fix

--- preprocess.c
+++ preprocess.c
@@ -4,6 +4,13 @@
 #include <stdbool.h>
 #include "preprocess.h"
 
+char* string_copy(const char* str_input) {
+    char* str_copy = (char *)malloc(strlen(str_input) + 1);
+    CHECK_ALLOCATION(str_copy);
+    strcpy(str_copy, str_input);
+    return str_copy;
+}
+
 bool is_it_a_macro_statement_line(char *line){
     /*TODO: see edge cases for macr string*/
     const char *macro_call = "macr";
@@ -21,27 +28,11 @@ bool is_it_a_macro_statement_line(char *line){
     return false;
 }
 
-int check_how_many_elements_in_line(char *line){
-    int c = 0;
-    char* token_ptr = strtok(line, " \t");
-    while (token_ptr)
-    {
-        c++;
-        token_ptr = strtok(NULL, " \t");
-    }
-    
-    return c;
-}
-
 
 char* find_macro_name(char* line){
-    char* macr_name_ptr = strtok(line, " \t");
+    char *line_copy = string_copy(line);
+    char* macr_name_ptr = strtok(line_copy, " \t");
     char* macro_name = (char*)malloc(MAX_LEN_MACRO_NAME*sizeof(char));
-
-    if (strncmp("macr", macr_name_ptr, 4) != 0){
-        printf("Error occurred while parse macro name\n");
-        exit(1);
-    }
 
     macr_name_ptr = strtok(NULL, " \t");
     if(macr_name_ptr != NULL){
@@ -52,12 +43,50 @@ char* find_macro_name(char* line){
         printf("Missing of macro name\n");
         exit(1);
     }
-    printf("%s\n*\n\n", macro_name);
+    free(line_copy);
     return macro_name;
 }
 
+int check_how_many_elements_in_line(char *line){
+    char *line_copy = string_copy(line);
+    int c = 0;
+    char* token_ptr = strtok(line_copy, " \t");
+    while (token_ptr)
+    {
+        c++;
+        token_ptr = strtok(NULL, " \t");
+    }
+    free(line_copy);
+    return c;
+}
+
+bool validation_check_of_macro_line(char* line){
+    char* line_copy = string_copy(line);
+    int num_of_elem_in_line = check_how_many_elements_in_line(line_copy);
+    char* macr_name_ptr = strtok(line_copy, " \t");
+
+    if (num_of_elem_in_line != 2){
+        printf("Check validation of macro statement line and try again\n");
+        free(line_copy);
+        return false;
+    }
+
+    if (strncmp("macr", macr_name_ptr, 4) != 0){
+        printf("Error occurred while parsing macro name\n");
+        free(line_copy);
+        return false;
+    }
+    free(line_copy);
+    return true;
+}
+
 bool check_validation_macro_line_add_macro_to_macros_list(char *line, macro** macro_list, int macro_counter){
-    char* macro_name = find_macro_name(line);
+    char* macro_name;
+    if(validation_check_of_macro_line(line)==false){
+        printf("invalid macro line, exiting");
+        exit(1);
+    }
+    macro_name = find_macro_name(line);
     printf("%c\n\n", macro_name[0]);
     /**macro_list = realloc(*macro_list, (macro_counter + 1) * sizeof(macro *));
     CHECK_ALLOCATION(*macro_list);*/
