66155b562f786e9154e99a5a035cc60ab47673c5
maayanh9 <maayanh9@gmail.com>
2024-09-09 14:15:50 +0300

    order the files and the makefile

---
+++ first_pass.h
@@ -0,0 +1,29 @@
+#ifndef FIRST_PASS_H
+#define FIRST_PASS_H
+
+#include "dynamic_list.h"
+
+
+bool assembler(const char *input_file_name);
+
+
+typedef struct ParsedDataOutput{
+    bool success;
+    DynamicList parsed_lines_list;
+    DynamicList symbols_table;
+    DynamicList errors_ptrs;
+    DynamicList entry_ptrs;
+    DynamicList external_ptrs;
+    DynamicList direct_labels_ptrs;
+} ParsedDataOutput;
+
+void free_parsed_data_output_dynamic_lists(DynamicList parsed_lines_list, DynamicList symbols_table, DynamicList errors_ptrs, DynamicList entry_ptrs, DynamicList external_ptrs, DynamicList direct_labels_ptrs);
+
+typedef enum{
+    DESTINATION = 1,
+    SOURCE
+}SourceOrDest;
+
+ParsedDataOutput first_pass(const char *input_file_name);
+
+#endif
